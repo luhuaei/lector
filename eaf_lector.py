dd6bb045fc3a0a3240df2015ad24d75328c27f73
luhuaei <luhuaei@gmail.com>
2021-08-15 19:31:56 +0800

    open url

--- eaf_lector.py
+++ eaf_lector.py
@@ -6,5 +6,6 @@ from app.lector.lector.mainui import MainUI
 class LectorWidget(MainUI):
     def __init__(self, url):
         super(LectorWidget, self).__init__()
+        self.process_post_hoc_files([url], True)
         self.show()
         self.resizeEvent()
